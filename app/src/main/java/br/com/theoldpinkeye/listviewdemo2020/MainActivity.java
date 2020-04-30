6e7746b82494a475d1041e893fba6145158a2a06
Danilo <danilo.dgdn@gmail.com>
2020-04-30 17:47:46 -0300

    primeiro commit

---
+++ app/src/main/java/br/com/theoldpinkeye/listviewdemo2020/MainActivity.java
@@ -0,0 +1,23 @@
+package br.com.theoldpinkeye.listviewdemo2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+
+        Button.
+    }
+}
